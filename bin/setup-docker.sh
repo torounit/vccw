58d36d25d705f8744b8d6e113cd520632a934959
miya0001 <miya@wpist.me>
2017-01-14 04:28:25 +0900

    some fix

--- bin/setup-docker.sh
+++ bin/setup-docker.sh
@@ -6,13 +6,23 @@ apt-get update -y
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:ansible/ansible
 apt-get update
-apt-get install -y --no-install-recommends ansible ruby curl
+apt-get install -y --no-install-recommends ansible ruby curl sudo
 apt-get clean
 
-cat << EOS > hosts
+cat << EOS > /home/ubuntu/hosts
 [default]
 localhost
 EOS
 
-ruby -ryaml -rjson -e "conf = {:vccw =>YAML.load(STDIN.read)}; conf[:vccw][:vagrant_dir] = \"/vagrant\"; puts JSON.generate(conf);" < /vagrant/provision/default.yml > test.json
-curl https://raw.githubusercontent.com/vccw-team/vccw-xenial64/master/provision/playbook.yml > middleware.yaml
+getent passwd ubuntu > /dev/null
+
+if [ $? -ne 0 ]; then
+  groupadd -g 1000 ubuntu
+  useradd -g ubuntu -G sudo -m -s /bin/bash ubuntu
+  echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+fi
+
+ruby -ryaml -rjson -e "conf = {:vccw =>YAML.load(STDIN.read)}; conf[:vccw][:vagrant_dir] = \"/vagrant\"; puts JSON.generate(conf);" < /vagrant/provision/default.yml > /home/ubuntu/test.json
+curl https://raw.githubusercontent.com/vccw-team/vccw-xenial64/master/provision/playbook.yml > /home/ubuntu/middleware.yaml
+
+chown -R ubuntu:ubuntu /home/ubuntu
