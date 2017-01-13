893fd52c400c1cc02f710bf1390cd5792b08e9b0
miya0001 <miya@wpist.me>
2017-01-14 04:39:31 +0900

    bug fix

--- bin/setup-docker.sh
+++ bin/setup-docker.sh
@@ -9,14 +9,14 @@ apt-get update
 apt-get install -y --no-install-recommends ansible ruby curl sudo
 apt-get clean
 
+groupadd -g 1000 ubuntu
+useradd -g ubuntu -G sudo -m -s /bin/bash ubuntu
+echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
 cat << EOS > /home/ubuntu/hosts
 [default]
 localhost
 EOS
-
-groupadd -g 1000 ubuntu
-useradd -g ubuntu -G sudo -m -s /bin/bash ubuntu
-echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 ruby -ryaml -rjson -e "conf = {:vccw =>YAML.load(STDIN.read)}; conf[:vccw][:vagrant_dir] = \"/vagrant\"; puts JSON.generate(conf);" < /vagrant/provision/default.yml > /home/ubuntu/test.json
 curl https://raw.githubusercontent.com/vccw-team/vccw-xenial64/master/provision/playbook.yml > /home/ubuntu/middleware.yaml
